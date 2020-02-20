571a529c3155b75741e6ad5425ce5fb9560d0122
Vladimir <rakicfolk@gmail.com>
2020-02-20 02:00:05 +0100

    first commit

---
+++ Test_Aplikacija/App_Start/WebApiConfig.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace Test_Aplikacija
+{
+    public static class WebApiConfig
+    {
+        public class CustomJsonFormatter : JsonMediaTypeFormatter
+        { 
+             public CustomJsonFormatter()
+        {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+        }
+
+        public static void Register(HttpConfiguration config)
+        {
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{Id}",
+                defaults: new { id = RouteParameter.Optional}
+            );
+            
+            //BELLOW CAMEl-CASE jSON REQUEST AND RESPONSE FORMAT
+            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.Add(new CustomJsonFormatter());
+        }
+    }
+}
---
+++ Test_Aplikacija/Controllers/EmployeeController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Web.Http;
+using Test_Aplikacija.Models;
+
+namespace Test_Aplikacija.Controllers
+{
+    
+    public class EmployeeController : ApiController
+    {
+        List<Employee> employees;
+        public EmployeeController()
+        {
+            employees = new List<Employee>();
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\vraki\Desktop\APP\Test_Aplikacija\EDIt\text\emp.txt");
+
+            foreach (var line in lines)
+            {
+                var props = line.Split(',');
+                employees.Add(new Employee()
+                {
+                    Id = Convert.ToInt32(props[0]),
+                    FirstName = props[1],
+                    LastName = props[2],
+                    Email = props[3],
+                    Image = props[4]
+                });
+            }
+        }
+        [HttpGet]
+        [Route("Employees/getAll")]
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return employees;
+        }
+      
+        public HttpResponseMessage GetEmployee(int id)
+        {
+            var employee = employees.FirstOrDefault((p) => p.Id == id);
+            if (employee == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id " + id.ToString() + " is not found");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, employee);
+        }
+        [HttpDelete]
+        [ActionName("deleteObject")]
+        public HttpResponseMessage EraseEmployee(int id) {
+
+           var emp = employees.FirstOrDefault((p) => p.Id == id);
+           if (emp == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "employee with Id = " + id.ToString() + "not found to delete");
+            }
+            else {
+
+                employees.Remove(emp);
+            }
+
+            List<string>lines= new List<string>();
+
+            foreach (var e in employees) {
+               var line = ""+e.Id+","+e.FirstName+","+e.LastName+","+ e.Email+","+ e.Image;
+                lines.Add(line);
+
+            }
+
+            System.IO.File.WriteAllLines(@"C:\Users\vraki\Desktop\APP\Test_Aplikacija\EDIt\text\emp.txt", lines.ToArray());
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [HttpPost]
+        [ActionName("insertObject")]
+        public HttpResponseMessage AddNewEmployee(Employee emp)
+        {
+            Random rnd = new Random();
+            emp.Id = rnd.Next(1, 100000);
+            employees.Add(emp);
+            List<string> lines = new List<string>();
+
+            try
+            {
+                foreach (var e in employees)
+                {
+                    var line = "" + e.Id + "," + e.FirstName + "," + e.LastName + "," + e.Email + "," + e.Image;
+                    lines.Add(line);
+                }
+                System.IO.File.WriteAllLines(@"C:\Users\vraki\Desktop\APP\Test_Aplikacija\EDIt\text\emp.txt", lines.ToArray());
+            }
+
+            catch (IOException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, emp.Id);
+        }
+
+        [HttpPatch]
+        [ActionName("udpadteObject")]
+        public HttpResponseMessage EditEmployees(Employee emp)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (var e in employees)
+            {
+                if (e.Id == emp.Id)
+                {
+                    e.FirstName = emp.FirstName;
+                    e.LastName = emp.LastName;
+                    e.Email = emp.Email;
+                    e.Image = emp.Image;
+                }
+                var line = "" + e.Id + "," + e.FirstName + "," + e.LastName + "," + e.Email + "," + e.Image;
+                lines.Add(line);
+            }
+
+            System.IO.File.WriteAllLines(@"C:\Users\vraki\Desktop\APP\Test_Aplikacija\EDIt\text\emp.txt", lines.ToArray());
+
+            return Request.CreateResponse(HttpStatusCode.OK, "User successfilly updated");
+        }
+
+
+    }
+}
